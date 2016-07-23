3316796e96bf22fc99d6b4123c68214543672f46
Pierre GILLON <gillon.pierre69@gmail.com>
2016-07-23 17:46:44 +0200

    Creation of the NotifiableObject

---
+++ Mvvm.Tests/NotifiableObjectTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using NFluent;
+using Xunit;
+
+namespace Mvvm.Tests
+{
+    public class NotifiableObject_should
+    {
+        [Fact]
+        public void return_affected_value()
+        {
+            var person = new Person();
+
+            person.FirstName = "Mark";
+
+            Check.That(person.FirstName).IsEqualTo("Mark");
+        }
+
+        [Fact]
+        public void raise_property_changed_when_setting_notifiable_property()
+        {
+            var person = new Person();
+            PropertyChangedEventArgs propertyChangedEventArgs = null;
+            person.PropertyChanged += (sender, args) => {
+                propertyChangedEventArgs = args;
+            };
+
+            person.FirstName = "Dupond";
+
+            Check.That(propertyChangedEventArgs).IsNotNull();
+            Check.That(propertyChangedEventArgs.PropertyName).IsEqualTo("FirstName");
+        }
+
+        [Fact]
+        public void not_raise_property_changed_when_setting_notifiable_property_with_same_values()
+        {
+            var person = new Person {FirstName = "Dupond"};
+            PropertyChangedEventArgs propertyChangedEventArgs = null;
+            person.PropertyChanged += (sender, args) => {
+                propertyChangedEventArgs = args;
+            };
+
+            person.FirstName = "Dupond";
+
+            Check.That(propertyChangedEventArgs).IsNull();
+        }
+
+        [Fact]
+        public void be_thread_safe()
+        {
+            var person = new Person();
+            var tasks = new List<Task>();
+            for (int i = 0; i < 10; i++) {
+                tasks.Add(Task.Factory.StartNew(() => person.FirstName = i.ToString()));
+            }
+            Task.WaitAll(tasks.ToArray());
+            Check.That(person.FirstName).IsNotNull();
+        }
+
+        private class Person : NotifiableObject
+        {
+            public string FirstName
+            {
+                get { return GetNotifiableProperty<string>(); }
+                set { SetNotifiableProperty(value); }
+            }
+        }
+    }
+}
---
+++ Mvvm/NotifiableObject.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Mvvm
+{
+    public class NotifiableObject : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        private readonly object _locker = new object();
+        private readonly IDictionary<string, object> _notifiableProperties = new Dictionary<string, object>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected virtual void RaisePropertyChanging([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        }
+
+        protected T GetNotifiableProperty<T>([CallerMemberName] string propertyName = null)
+        {
+            if (propertyName == null) throw new ArgumentNullException(nameof(propertyName));
+            object value;
+            if (_notifiableProperties.TryGetValue(propertyName, out value)) {
+                return (T)value;
+            }
+            return default(T);
+        }
+        protected void SetNotifiableProperty<T>(T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (propertyName == null) throw new ArgumentNullException(nameof(propertyName));
+            lock (_locker) {
+                if (_notifiableProperties.ContainsKey(propertyName) == false) {
+                    _notifiableProperties.Add(propertyName, null);
+                }
+                var oldValue = (T) _notifiableProperties[propertyName];
+                if (Equals(oldValue, newValue) == false) {
+                    RaisePropertyChanging(propertyName);
+                    _notifiableProperties[propertyName] = newValue;
+                    RaisePropertyChanged(propertyName);
+                }
+            }
+        }
+    }
+}
